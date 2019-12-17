91d631fcaa21706887236ce9edc9f0eef630e741
Divyesh Hadvani <34337542+DivyeshHadvani@users.noreply.github.com>
2019-12-17 15:17:04 +0530

    Add files via upload

---
+++ Convert_Char_ASCII_Arduino_Serial.ino
@@ -0,0 +1,19 @@
+String incoming;   // for incoming serial data
+
+void setup() {
+    Serial.begin(9600);  // opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+
+    // send data only when you receive data:
+    if (Serial.available() > 0) 
+    {
+            // read the incoming byte:
+            incoming = Serial.read();
+            String mysrr = String(incoming);
+            // say what you got:
+            Serial.print("I received: ");
+            Serial.println(mysrr);
+    }
+}
